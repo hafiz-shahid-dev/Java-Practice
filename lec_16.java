aa203862a2bf625222c4d1679af75cf30d242d5e
Hafiz Shahid <hafizshahid.dev@gmail.com>
2025-10-16 11:07:32 -0700

    solve program

--- lec_16.java
+++ lec_16.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 public class lec_16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // System.out.println("Enter a number");
+     //   int num = sc.nextInt();
+    //    if (num % 2 == 0)
+     //   System.out.println(num + "is a even. ");
+    //   else{
+      // System.out.println(num + "is a odd. ");
 
-    public static void main(String []args){
 
-        System.out.println("Hafiz is java developer");
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        if(num>= 0) 
+            System.out.println(num + "is a positive");
+            else {
+                System.out.println(num + "is a negative");
+        }
     }
 }
+
--- lec_17.java
+++ lec_17.java
@@ -1,6 +1,6 @@
 public class lec_17 {
     public static void main(String []args){
 
-        System.out.println("Hafiz is java developer lec 17");
+
     }
 }
