815b207e94eab74719e5a53b1a6a9878442e1cda
Hafiz Shahid <hafizshahid.dev@gmail.com>
2025-10-22 11:35:42 -0700

    while loop

---
+++ While.java
@@ -0,0 +1,28 @@
+public class While {
+    public static void main(String[] args) {
+        // int num = 100;
+//        while(num<=200 ) {
+//            System.out.println(num);
+//            num++;
+//        }
+//    }
+//}
+//    while(num<=200) {
+//        if (num % 2 == 0) {
+//            System.out.println(num);
+//        }
+//            num++;
+//        }
+//        }
+//    }
+
+        int i = 1;
+        int sum = 0;
+        while (i <= 5) {
+            sum = sum + i;
+            i++;
+        }
+            System.out.println("sum = " + sum);
+
+    }
+}
