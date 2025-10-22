766c39e916e19f15e9536ef3c830d1efc502e614
Hafiz Shahid <hafizshahid.dev@gmail.com>
2025-10-22 02:26:31 -0700

    switch

--- elseif.java
+++ elseif.java
@@ -6,13 +6,26 @@ public class elseif {
         int age;
         age = sc.nextInt();
 
-        if (age > 50) {
-            System.out.println("you are experienced!");
-        } else if (age > 40) {
-            System.out.println("you are better experienced!");
-        } else if (age > 35) {
-        } else {
-            System.out.println("you are not experienced");
+       // if (age > 50) {
+            //System.out.println("you are experienced!");
+     //   } else if (age > 40) {
+          //  System.out.println("you are better experienced!");
+       // } else if (age > 35) {
+       // } else {
+          //  System.out.println("you are not experienced");
+       // }
+        switch(age) {
+            case 18:
+                System.out.println("you are adult");
+                break;
+            case 25:
+                System.out.println("you are healthy");
+                break;
+            case 35:
+                System.out.println("you are not healthy");
+                break;
+            default:
+                System.out.println("you are not adult");
         }
     }
 }
