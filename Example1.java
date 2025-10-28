058a52362073d7d26c2052530734c5e6b667909b
Hafiz Shahid <hafizshahid.dev@gmail.com>
2025-10-28 11:18:09 -0700

    example 01

---
+++ Example1.java
@@ -0,0 +1,29 @@
+public class Example1 {
+    public static void main(String[] args) {
+//        int n = 4;
+//        for(int i = 0; i<0; i--) {
+//            for(int j = 0; j<i; j++ ) {
+//                System.out.println("*");
+//            }
+//            System.out.println("/n");
+//        }
+
+//        int sum = 0;
+//        int n = 6;
+//        for(int i = 0; i<n; i++) {
+//            sum = sum + (2*i);
+//            System.out.print("sum of even number is:");
+//            System.out.println(sum);
+//        }
+
+//        int n = 6;
+//        for(int i=1;i<=10;i++){
+//            System.out.printf("%d X %d  =  %d\n " , n, i, n*i);
+//        }
+        int n = 10;
+        for (int i = 10; i >= 1; i--) {
+            System.out.printf("%d X %d  =  %d\n ", n, i, n * i);
+
+        }
+    }
+}
