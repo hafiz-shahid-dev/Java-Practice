3a97d7feff14607f67b5b066ed6e2f1ec5c14f11
Hafiz Shahid <hafizshahid.dev@gmail.com>
2025-10-29 11:26:53 -0700

    example2

---
+++ Example2.java
@@ -0,0 +1,29 @@
+public class Example2 {
+    public static void main(String[] args) {
+//        int n = 5;
+//        int i = 1;
+//        int factorial = 1;
+//        while (i <= n) {
+//            factorial *= i;
+//            i++;
+//            System.out.println(factorial);
+//        }
+
+//        int n = 8;
+//        int sum = 0;
+//        for(int i=1; i<=10; i++){
+//             sum += n*i;
+//             System.out.println(sum);
+//        }
+        int []marks;
+       marks =  new int [5];
+       marks[0] = 100;
+       marks[1] = 90;
+       marks[2] = 80;
+       marks[3] = 70;
+       marks[4] = 60;
+       System.out.println(marks[3]);
+
+    }
+}
+
