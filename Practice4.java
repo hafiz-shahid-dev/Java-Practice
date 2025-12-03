809777f96a6ad9151ce346afde5765c2bd456b39
Hafiz Shahid <hafizshahid.dev@gmail.com>
2025-12-03 00:28:57 -0800

    practice4

---
+++ Practice4.java
@@ -0,0 +1,87 @@
+// class Employee {
+//     int salary;
+//     String name;
+//
+//     int getSalary() {
+//         return salary;
+//     }
+//
+//     String getName() {
+//         return name;
+//     }
+//
+//     void setName(String newName) {
+//         name = newName;
+//     }
+// }
+//    public class Practice4 {
+//    public static void main(String[] args) {
+//        Employee emp1 = new Employee();
+//        emp1.name = "Ali";
+//        emp1.salary = 5000;
+//        System.out.println("Employee NAME:" + emp1.getName());
+//        System.out.println("Employee:" + emp1.getSalary());
+//        emp1.setName("Ahmad");
+//        System.out.println("Updated Name:" + emp1.getName());
+//
+//    }
+//class CellPhone{
+//    public void ring() {
+//        System.out.println("Ringing...");
+//    }
+//    public void vibrate() {
+//        System.out.println("Vibrating...");
+//    }
+//    public void silent() {
+//        System.out.println("Silent mode on...");
+//    }
+//
+//    }
+//    public class Practice4 {
+//        public static void main(String[] args) {
+//            CellPhone phone = new CellPhone();
+//            phone.ring();
+//            phone.vibrate();
+//            phone.silent();
+//        }
+//    }
+//class Square {
+//    int side;
+//
+//    void setSide(int s) {
+//        side = s;
+//    }
+//
+//    int area() {
+//        return side * side;
+//    }
+//
+//    int perimeter() {
+//        return 4 * side;
+//    }
+//}
+class Rectangle {
+    int length = 1;
+    int width = w;
+    void setValues(int 1, int w) {
+        length = 1;
+        width = w;
+    }
+    int area() {
+        return length * width;
+    }
+    int perimeter() {
+        return 2 * (length + width);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
